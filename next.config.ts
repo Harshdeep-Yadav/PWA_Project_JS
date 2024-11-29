7c2dfd67a0b45a2834f6ccd8ac08c430a13cfcf3
Harshdeep Yadav <harshdeepy5708@gmail.com>
2024-11-29 15:24:33 +0530

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
 
-export default nextConfig;
+// export default nextConfig;
+
+
+// next.config.js
+const withPWA = require('next-pwa')({
+  dest: 'public',          // Output directory for the service worker
+  register: true,          // Automatically register the service worker
+  skipWaiting: true,       // Activate updated service worker immediately
+  // disable: process.env.NODE_ENV === 'development', // Disable PWA during development
+});
+
+module.exports = withPWA({
+  reactStrictMode: true,
+  // Other Next.js configurations
+});
